bb06128d685ae98fbbee0396dd04e545326735f8
Benjamin Tsai <coding47414@gmail.com>
2024-11-16 23:54:17 +0800

    0

--- budget-assistant/src/AddTransaction.jsx
+++ budget-assistant/src/AddTransaction.jsx
@@ -162,25 +162,26 @@ function AddTransactionWithDate() {
     );
 
     return (
-        <div  style ={{
-                height: 'auto',
-                minHeight: '100vh',
-                overflowY: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                padding: '20px',
-                boxSizing: 'border-box',
-            }}>
+        <div style={{
+            height: 'auto',
+            minHeight: '100vh',
+            overflowY: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '20px',
+            boxSizing: 'border-box',
+        }}>
             <button onClick={handleLogout}>Log out</button>
             <h2>請選擇日期，紀錄您的帳務~</h2>
 
-            <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                dateFormat="yyyy/MM/dd"
-                inline
-            />
-
+            <div className='date-picker-container'>
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={(date) => setSelectedDate(date)}
+                    dateFormat="yyyy/MM/dd"
+                    inline
+                />
+            </div>
             <form onSubmit={handleSubmit}>
                 <label>
                     金額(Amount):
