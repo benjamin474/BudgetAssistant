aa6ef632ce7b18a3129c8693c9ca089981cfd061
Benjamin Tsai <coding47414@gmail.com>
2024-11-16 23:40:12 +0800

    還是不行

--- budget-assistant/src/AddTransaction.jsx
+++ budget-assistant/src/AddTransaction.jsx
@@ -162,7 +162,15 @@ function AddTransactionWithDate() {
     );
 
     return (
-        <div className='page-container'>
+        <div  style ={{
+                height: 'auto',
+                minHeight: '100vh',
+                overflowY: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+                padding: '20px',
+                boxSizing: 'border-box',
+            }}>
             <button onClick={handleLogout}>Log out</button>
             <h2>請選擇日期，紀錄您的帳務~</h2>
 
