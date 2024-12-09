078e34b10a963686d454175a2ffcf85fb7d61db5
pizzysiuuu <vinnie.halim227@gmail.com>
2024-12-10 00:33:14 +0800

    login signup ui

--- budget-assistant/src/Page/Signup.jsx
+++ budget-assistant/src/Page/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../style/Signup.css';
 
 const Signup = () => {
@@ -58,66 +58,81 @@ const Signup = () => {
   };
 
   return (
-    <div className="signup-container">
-      <h2>註冊帳號</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">電子郵件:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="輸入您的電子郵件"
-            required
-          />
-        </div>
+    <div className='signup-page'>
+      <div className="signup-container">
+        <h2>註冊帳號</h2>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">電子郵件:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="輸入您的電子郵件"
+              required
+              className="form-input"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="username">用戶名:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            placeholder="輸入您的用戶名"
-            required
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="username">用戶名:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="輸入您的用戶名"
+              required
+              className="form-input"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="password">密碼:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="輸入您的密碼"
-            required
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="password">密碼:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="輸入您的密碼"
+              required
+              className="form-input"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="password2">確認密碼:</label>
-          <input
-            type="password"
-            id="password2"
-            name="password2"
-            value={formData.password2}
-            onChange={handleChange}
-            placeholder="再次輸入您的密碼"
-            required
-          />
+          <div className="form-group">
+            <label htmlFor="password2">確認密碼:</label>
+            <input
+              type="password"
+              id="password2"
+              name="password2"
+              value={formData.password2}
+              onChange={handleChange}
+              placeholder="再次輸入您的密碼"
+              required
+              className="form-input"
+            />
+          </div>
+          <button type="submit" className="signup-button">
+            註冊
+          </button>
+        </form>
+        <div className="link">
+            <span>Already have an account?</span> <Link to="/login" className="login-link">Log in</Link>
         </div>
-        
-        <button type="submit" className="login-button">
-          註冊
-        </button>
-        <button onClick={handleBack} className='login-button'>返回</button>
-      </form>
+      </div>
+      <div className="signup-image-container">
+          <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              alt="Signup illustration"
+              className="signup-image"
+          />
+      </div>
     </div>
   );
 };
