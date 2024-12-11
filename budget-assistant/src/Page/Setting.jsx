0096fefd28abe7637117d9c114dbfdedec91b0e9
aaronwu2 <aaronwu239@gmail.com>
2024-12-11 23:59:03 +0800

    done haole

--- budget-assistant/src/Page/Setting.jsx
+++ budget-assistant/src/Page/Setting.jsx
@@ -55,6 +55,17 @@ const Setting = () => {
                         {isEditingPassword ? '取消' : '修改'}
                     </button>
                 </p>
+                <p>
+                       {/* Google Login Button */}
+                       <button className="google-login-button" onClick={handleGoogleLogin}>
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+                        alt="Google Logo"
+                        className="google-logo"
+                    />
+                    Log in with Google
+                    </button>
+                </p>
             </div>
             {isEditingUsername && (
                 <form onSubmit={(e) => handleUsernameUpdate(e, newUsername, token, setNewUsername)} className="settings-form">
